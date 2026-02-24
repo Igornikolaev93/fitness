0ece055da6da870df2679a470d876f5537d9a5fe
igorn2659 <igorn2659@gmail.com>
2026-02-24 12:12:06 +0000

    addfiles

--- src/pages/ClientsPage.vue
+++ src/pages/ClientsPage.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>Clients</h1>
-    <ClientList />
+    <ClientsList />
     <WorkoutList />
     <AssignWorkout />
   </div>
 </template>
 
 <script setup>
-import ClientList from '@/components/ClientList.vue'
+import ClientsList from '@/components/ClientsList.vue'
 import WorkoutList from '@/components/WorkoutList.vue'
 import AssignWorkout from '@/components/AssignWorkout.vue'
 </script>
